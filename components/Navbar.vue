f0060e60af56548d936d2f923f0033d683d25b31
John Crane <john.crane@gmail.com>
2024-10-10 15:39:30 -0700

    changed title in nav bar to "App"

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-purple-500 text-white p-4 flex justify-between items-center">
     <nuxt-link to="/">
-      <h1 class="text-xl font-semibold">Habit Tracker</h1>
+      <h1 class="text-xl font-semibold">App</h1>
     </nuxt-link>
     <div>
 
